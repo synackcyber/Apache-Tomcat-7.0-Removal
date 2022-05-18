b24f2a3ff8b16bae080a82fbd05e2588ff09a3a6
Matt A <marsenault89@gmail.com>
2022-05-18 15:25:46 -0400

    Update ApacheCheckandRemove.ps1

--- ApacheCheckandRemove.ps1
+++ ApacheCheckandRemove.ps1
@@ -1,6 +1,6 @@
-## Test Path Vars
-$pathtest= "C:\Program Files (x86)\Apache Software Foundation\Tomcat 7.0\"
-$condition= Test-Path -Path $pathtest
+## Test Uninstall Path
+$pathtest = "C:\Program Files (x86)\Apache Software Foundation\Tomcat 7.0\Uninstall.exe"
+$condition = Test-Path -Path $pathtest
 if ( $condition )
 {
     Write-Output "Directory Found"
@@ -10,9 +10,21 @@ if ( $condition )
         exit
     }
 
+## Test Log Path
+$logpathtest = "C:\Program Files (x86)\Apache Software Foundation\Tomcat 7.0\logs\"
+$logcondition = Test-Path -Path $logpathtest
+if ( $logcondition )
+{
+    Write-Output "Log Directory Found"
+}
+    else {
+        Write-Host "Log Directory not found. Exiting."
+        exit
+    }
+
 ## File date check Vars
-$days_to_check=$(Get-Date).AddDays(-365)
-$getfile=Get-Item "C:\Program Files (x86)\Apache Software Foundation\Tomcat 7.0\logs\*.*" | where { $_.LastWriteTime -gt $days_to_check } | Foreach {
+$days_to_check = $(Get-Date).AddDays(-365)
+$getfile = Get-Item "C:\Program Files (x86)\Apache Software Foundation\Tomcat 7.0\logs\*.*" | where { $_.LastWriteTime -gt $days_to_check } | Foreach {
 “File Name: ” + $_.Name
 }
 
@@ -29,10 +41,6 @@ if ($getfile -gt 1) {
         cd "C:\Program Files (x86)\Apache Software Foundation\Tomcat 7.0\"
         Start-Sleep -Seconds 5
         ./Uninstall.exe /S -ServiceName="Tomcat7"
-        cd C:\
-        Start-Sleep -Seconds 30
-        rmdir -r "C:\Program Files (x86)\Apache Software Foundation\"
-        Write-Host "Removal of Apache Tomcat 7 Complete"
     }
 
 ## Backup Registry entires for TomCat7 Services.
@@ -41,10 +49,15 @@ $regtestpath = test-path -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tomcat7"
 if ($regtestpath) {
     reg export 'HKLM\SYSTEM\CurrentControlSet\Services\Tomcat7' C:\Temp\apacheRegKey_bak.reg
     Start-Sleep -Seconds 5
-    Remove-Item -Path "HKLM\SYSTEM\CurrentControlSet\Services\Tomcat7" -Recurse -Force
+    Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tomcat7" -Recurse -Force
     Write-Host "Registry entry has been backed up to C:\Temp and deleted. Please reboot PC at earliest convenience to remove Apache7 from services list."
 }
     else {
         Write-Host "Registry Key not found. Backup and removal failed"
         exit
     }
+
+## Remove Apache Directory
+cd C:\
+rmdir -r "C:\Program Files (x86)\Apache Software Foundation\"
+Write-Host "Removal of Apache Tomcat 7 Complete"
